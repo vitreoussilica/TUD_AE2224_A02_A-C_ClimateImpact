4aecb822b2ad4f606aa2fd3691a35f3c0e8f05fb
GerbenDr <90692838+GerbenDr@users.noreply.github.com>
2023-03-20 11:51:08 +0100

    changes

--- dataAnalysisCode/python/v1/flight.py
+++ dataAnalysisCode/python/v1/flight.py
@@ -7,8 +7,25 @@ class Flight:
         self.optimisation = optimisation
         self.route = route
 
+
+    def total_ATR_NOX_Ozone(self):
+        return self.data[10,:].sum()
+    
+    def total_ATR_NOX_Methane(self):
+        return self.data[11,:].sum()
+    
+    def total_ATR_Water_Vapour(self):
+        return self.data[12,:].sum()
+    
+    def total_ATR_Contrails(self):
+        return self.data[13,:].sum()
+    
+    def total_ATR_CO2(self):
+        return self.data[14,:].sum()
+    
     def total_ATR(self):
         return self.data[15,:].sum()
+
     
     def print_summary(self):
         print(f'Flight data shape: {self.data.shape}')
